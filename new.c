022e3c3c92688baf085f676a2e3344ef8a51cb1d
alsshsskssl <alsshsskssl@gmail.com>
2019-05-14 14:54:00 +0900

    hi

--- new.c
+++ new.c
@@ -7,18 +7,16 @@
 int main(){
   if(wiringPiSetup() == -1)
  	  return 1;
-  pinMode (LED1, PWM_OUTPUT);
+  pinMode (LED1, OUTPUT);
   pinMode (LED2, OUTPUT);
 
   digitalWrite(LED1, 1);
-  
-  pwmSetClock(19);
-  pwmSetMode(PWM_MODE_MS);
-
-  pwmSetRange(4000000);
-  pwmWrite(1, 2000000);
-  
   while(1){
+    digitalWrite(LED1, 0);
+    delay(2000);
+    digitalWrite(LED1, 1);
+    delay(2000);
+
     int sw_in_val = digitalRead(BUTTON);
     if(!sw_in_val){
       for(int j=0; j<5; j++){
